ee1b3a5b6f62959e83118a91ca39a855e6c894b9
“FerryMan” <overzetter@gmail.com>
2021-06-12 19:36:17 +0200

    Console app die strings manipuleert.
    Nog niet unit testable:
    1. geen classes buiten Program,
    2. static methods,
    3. methods zonder parameters en returntype void.
    Refactoring nodig om applicatie unit testable te maken.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//demo from https://wellsb.com/csharp/
+using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Reverse String");
+            Console.WriteLine("2) Remove Whitespace");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ReverseString();
+                    return true;
+                case "2":
+                    RemoveWhitespace();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static string CaptureInput()
+        {
+            Console.Write("Enter the string you want to modify: ");
+            return Console.ReadLine();
+        }
+
+        private static void ReverseString()
+        {
+            Console.Clear();
+            Console.WriteLine("Reverse String");
+
+            char[] charArray = CaptureInput().ToCharArray();
+            Array.Reverse(charArray);
+            DisplayResult(String.Concat(charArray));
+        }
+
+        private static void RemoveWhitespace()
+        {
+            Console.Clear();
+            Console.WriteLine("Remove Whitespace");
+
+            DisplayResult(CaptureInput().Replace(" ", ""));
+        }
+
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine($"\r\nYour modified string is: {message}");
+            Console.Write("\r\nPress Enter to return to Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
